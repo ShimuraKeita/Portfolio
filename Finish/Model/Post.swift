a83bfc1e8c2419c19492ac3efd89fdb9d897e1cc
ShimuraKeita <keitagsk8001@gmail.com>
2021-03-16 23:02:58 +0900

    Post Model 作成

---
+++ Finish/Model/Post.swift
@@ -0,0 +1,36 @@
+//
+//  Post.swift
+//  Finish
+//
+//  Created by 志村　啓太 on 2021/03/16.
+//
+
+import Foundation
+
+struct Post {
+    let caption: String
+    let postID: String
+    var likes: Int
+    var timestamp: Date!
+    var user: User
+    var didLike = false
+    var replyingTo: String?
+    
+    var isReply: Bool { return replyingTo != nil }
+    
+    init(user: User, postID: String, dictionary: [String: Any]) {
+        self.postID = postID
+        self.user = user
+        
+        self.caption = dictionary["caption"] as? String ?? ""
+        self.likes = dictionary["likes"] as? Int ?? 0
+
+        if let timestamp = dictionary["timestamp"] as? Double {
+            self.timestamp = Date(timeIntervalSince1970: timestamp)
+        }
+        
+        if let replyingTo = dictionary["replyingTo"] as? String {
+            self.replyingTo = replyingTo
+        }
+    }
+}
