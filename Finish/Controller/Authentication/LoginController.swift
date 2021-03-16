ac9a4a7eafa8f4db69a727eabd0ef63b1704543d
ShimuraKeita <keitagsk8001@gmail.com>
2021-03-16 11:44:59 +0900

    Login UI 作成

--- Finish/Controller/Authentication/LoginController.swift
+++ Finish/Controller/Authentication/LoginController.swift
@@ -18,6 +18,35 @@ class LoginController: UIViewController {
         return iv
     }()
     
+    private let emailTextField = CustomTextField(placeholder: "メールアドレス")
+    private let passwordTextField: CustomTextField = {
+        let tf = CustomTextField(placeholder: "パスワード（半角英数字/6文字以上）", isSecureField: true)
+        tf.keyboardType = .emailAddress
+        return tf
+    }()
+    
+    private let loginButton: CustomButton = {
+        let button = CustomButton(title: "ログイン", type: .system)
+//        button.addTarget(self, action: #selector(handleLogin), for: .touchUpInside)
+        return button
+    }()
+    
+    private let forgotPasswordButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("パスワードをお忘れですか？", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+        button.setTitleColor(.white, for: .normal)
+//        button.addTarget(self, action: #selector(handleShowLoginForgotPassword), for: .touchUpInside)
+        return button
+    }()
+
+    private let goToRegistrationButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.attributedTitle(firstPart: "新規アカウント作成は", secondPart: "こちら")
+//        button.addTarget(self, action: #selector(handleShowRegistration), for: .touchUpInside)
+        return button
+    }()
+    
     //MARK: - Lifecyle
     
     override func viewDidLoad() {
@@ -27,6 +56,18 @@ class LoginController: UIViewController {
     }
     
     //MARK: - Selectors
+    
+    @objc func handleLogin() {
+        
+    }
+    
+    @objc func handleShowLoginForgotPassword() {
+        
+    }
+    
+    @objc func handleShowRegistration() {
+        
+    }
     
     //MARK: - Helpers
     
@@ -39,5 +80,19 @@ class LoginController: UIViewController {
         iconImage.centerX(inView: view)
         iconImage.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 32)
         iconImage.setDimensions(width: 120, height: 120)
+        
+        let stack = UIStackView(arrangedSubviews: [emailTextField, passwordTextField,
+                                                   loginButton, forgotPasswordButton])
+        stack.axis = .vertical
+        stack.spacing = 16
+        
+        view.addSubview(stack)
+        stack.anchor(top: iconImage.bottomAnchor, left: view.leftAnchor,
+                     right: view.rightAnchor, paddingTop: 24, paddingLeft: 32, paddingRight: 32)
+        
+        view.addSubview(goToRegistrationButton)
+        goToRegistrationButton.anchor(left: view.leftAnchor,
+                                      bottom: view.safeAreaLayoutGuide.bottomAnchor,
+                                      right: view.rightAnchor, paddingLeft: 32, paddingBottom: 16, paddingRight: 32)
     }
 }
