615e5d1ad3c003f899898d9fd507c06e792aa00e
ShimuraKeita <keitagsk8001@gmail.com>
2021-03-16 10:36:23 +0900

    Root ViewController セットアップ

---
+++ Finish/Controller/MainTabController.swift
@@ -0,0 +1,15 @@
+//
+//  MainTabController.swift
+//  Finish
+//
+//  Created by 志村　啓太 on 2021/03/16.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
