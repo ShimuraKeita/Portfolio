4d2cd4627dad086025b05e99caeb5d64bbf70630
ShimuraKeita <keitagsk8001@gmail.com>
2021-03-16 20:08:00 +0900

    User Model 作成

---
+++ Finish/Model/User.swift
@@ -0,0 +1,44 @@
+//
+//  User.swift
+//  Finish
+//
+//  Created by 志村　啓太 on 2021/03/16.
+//
+
+import Foundation
+import Firebase
+
+struct User {
+    var fullname: String
+    let email: String
+    var username: String
+    var profileImageUrl: URL?
+    let uid: String
+    var isFollowed = false
+    var stats: UserRelationStats?
+    var sick: String
+    var bio: String
+    
+    var isCurrentUser: Bool { return Auth.auth().currentUser?.uid == uid }
+    
+    init(uid: String, dictionary: [String: AnyObject]) {
+        self.uid = uid
+        
+        self.fullname = dictionary["fullname"] as? String ?? ""
+        self.email = dictionary["email"] as? String ?? ""
+        self.username = dictionary["username"] as? String ?? ""
+        self.sick = dictionary["sick"] as? String ?? ""
+        self.bio = dictionary["bio"] as? String ?? ""
+        
+        if let profileImageUrlString = dictionary["profileImageUrl"] as? String {
+            guard let url = URL(string: profileImageUrlString) else { return }
+            self.profileImageUrl = url
+        }
+    }
+}
+
+struct UserRelationStats {
+    var followers: Int
+    var following: Int
+    let posts: Int
+}
