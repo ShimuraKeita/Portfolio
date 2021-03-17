2dd8075b22e8114fdf2026259566613bce7d69b4
ShimuraKeita <keitagsk8001@gmail.com>
2021-03-17 22:04:01 +0900

    UploadTweetViewModel 作成

---
+++ Finish/API/UploadPostViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  UploadPostViewModel.swift
+//  Finish
+//
+//  Created by 志村　啓太 on 2021/03/17.
+//
+
+import UIKit
+
+enum UploadTweetConfiguration {
+    case post
+    case reply(Post)
+}
+
+struct UploadTweetViewModel {
+    
+    let actionButtonTitle: String
+    let placeholderText: String
+    var shouldShowReplyLabel: Bool
+    var replyText: String?
+    
+    init(config: UploadTweetConfiguration) {
+        switch config {
+        case .post:
+            actionButtonTitle = "投稿"
+            placeholderText = "いまどうしてる?"
+            shouldShowReplyLabel = false
+        case .reply(let tweet):
+            actionButtonTitle = "返信"
+            placeholderText = "返信を投稿"
+            shouldShowReplyLabel = true
+            replyText = "返信先: @\(tweet.user.username) さん"
+        }
+    }
+}
