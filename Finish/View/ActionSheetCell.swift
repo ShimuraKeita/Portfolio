30413f0ef946478de8b1942da5623276d68e8ebc
ShimuraKeita <keitagsk8001@gmail.com>
2021-03-17 23:45:23 +0900

    Action Sheet Cell & Cancel Button 作成

---
+++ Finish/View/ActionSheetCell.swift
@@ -0,0 +1,55 @@
+//
+//  ActionSheetCell.swift
+//  Finish
+//
+//  Created by 志村　啓太 on 2021/03/17.
+//
+
+import UIKit
+
+class ActionSheetCell: UITableViewCell {
+    
+    // MARK: - Properties
+    
+    private let optionImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.clipsToBounds = true
+        iv.image = UIImage(systemName: "bubble.right")
+        iv.tintColor = .systemPink
+        return iv
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 18)
+        return label
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        backgroundColor = UIColor(named: "backgroundColor")
+        
+        addSubview(optionImageView)
+        optionImageView.centerY(inView: self)
+        optionImageView.anchor(left: leftAnchor, paddingLeft: 8)
+        optionImageView.setDimensions(width: 36, height: 36)
+        
+        addSubview(titleLabel)
+        titleLabel.centerY(inView: self)
+        titleLabel.anchor(left: optionImageView.rightAnchor, paddingLeft: 12)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Helpers
+    
+    func configure() {
+
+    }
+}
