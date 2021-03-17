dc9d8d53d775cae1fdc805d81420c63225ef8674
ShimuraKeita <keitagsk8001@gmail.com>
2021-03-17 22:19:28 +0900

    UploadPostController ReplyLabel作成

--- Finish/Controller/UploadPostController.swift
+++ Finish/Controller/UploadPostController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import ActiveLabel
 
 class UploadPostController: UIViewController {
     
@@ -40,6 +41,15 @@ class UploadPostController: UIViewController {
         return iv
     }()
     
+    private lazy var replyLabel: ActiveLabel = {
+        let label = ActiveLabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textColor = .lightGray
+        label.mentionColor = .systemPink
+        label.setWidth(width: view.frame.width)
+        return label
+    }()
+    
     private let captionTextView = InputTextView()
     
     //MARK: - Lifecycle
@@ -58,7 +68,6 @@ class UploadPostController: UIViewController {
         super.viewDidLoad()
         
         configureUI()
-        configureNavigationBar()
         
         switch config {
         
@@ -94,17 +103,30 @@ class UploadPostController: UIViewController {
     func configureUI() {
         view.backgroundColor = UIColor(named: "backgroundColor")
         
+        configureNavigationBar()
+        
         let imageCaptionStack = UIStackView(arrangedSubviews: [profileImageView, captionTextView])
         imageCaptionStack.axis = .horizontal
         imageCaptionStack.spacing = 12
         imageCaptionStack.alignment = .leading
-
-        view.addSubview(imageCaptionStack)
-        imageCaptionStack.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.leftAnchor,
-                                 right: view.rightAnchor, paddingTop: 16, paddingLeft: 16,
-                                 paddingRight: 16)
+        
+        let stack = UIStackView(arrangedSubviews: [replyLabel, imageCaptionStack])
+        stack.axis = .vertical
+        stack.spacing = 12
+        
+        view.addSubview(stack)
+        stack.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.leftAnchor,
+                     right: view.rightAnchor, paddingTop: 16, paddingLeft: 16,
+                     paddingRight: 16)
         
         profileImageView.sd_setImage(with: user.profileImageUrl, completed: nil)
+        
+        actionButton.setTitle(viewModel.actionButtonTitle, for: .normal)
+        captionTextView.placeholderLabel.text = viewModel.placeholderText
+        
+        replyLabel.isHidden = !viewModel.shouldShowReplyLabel
+        guard let replyText = viewModel.replyText else { return }
+        replyLabel.text = replyText
     }
     
     func configureNavigationBar() {
